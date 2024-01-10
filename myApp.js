5e8c7ab56b9d2d01a5690be5ca13e7e55ff2966f
Ahmed <ahmed.m112000@hotmail.com>
2024-01-10 23:11:20 +0400

    fix: attempt to fix file response

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ console.log('Hello World');
 
 const absolutePath = __dirname + '/views/index.html';
 
-app.get(absolutePath, (req, res) => {
+app.get('/', (req, res) => {
     res.send(absolutePath);
 });
 
