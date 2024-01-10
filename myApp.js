aa71fb847cd30689470ffdd005c8609b2c1f8763
Ahmed <ahmed.m112000@hotmail.com>
2024-01-10 23:14:25 +0400

    fix: attempt 2

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log('Hello World');
 const absolutePath = __dirname + '/views/index.html';
 
 app.get('/', (req, res) => {
-    res.send(absolutePath);
+    res.sendFile(absolutePath);
 });
 
 module.exports = app;
